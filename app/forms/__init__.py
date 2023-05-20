7140a89504dc4e0ac8595c6782ca31e6d959325b
Negalbert <sammynegalbert2@gmail.com>
2023-05-20 14:12:40 +0200

    urls d'authentification

--- app/forms/__init__.py
+++
@@ -1,2 +0,0 @@
-from .login_form import LoginForm
-from .signUp_form import SignUpForm
--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home, logout,signUp,login
+from .views import home
 urlpatterns = [
     path('',home.index,name='home_index' ),
     # ============== login page =============
