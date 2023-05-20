293eacc904eb5bdffcfc1e6ee77994d0e6bf0bac
Negalbert <sammynegalbert2@gmail.com>
2023-05-20 11:06:27 +0200

    les urls vers les pages d'authetification

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from .views import home
+from .views import home, logout,signUp
 urlpatterns = [
     path('',home.index,name='home_index' ),
+    # ============== login page =============
+    path('login', logout.index, name='login_index'),
+    # ============== signUp page =============
+    path('signUp', signUp.index, name='signUp_index')
 ]
