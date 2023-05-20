dedb8fc20daee85778e86184b035306bbdb12805
Negalbert <sammynegalbert2@gmail.com>
2023-05-20 13:31:59 +0200

    les urls pour la connexion des utilisateur et aussi l'enregistrement

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import home, logout,signUp
+from .views import home, logout,signUp,login
 urlpatterns = [
     path('',home.index,name='home_index' ),
     # ============== login page =============
     path('login', logout.index, name='login_index'),
+    path('login/user', login.loginUser, name="login_user"),
     # ============== signUp page =============
-    path('signUp', signUp.index, name='signUp_index')
+    path('signUp', signUp.index, name='signUp_index'),
+    path('signUp/user', signUp.save, name='save_user')
 ]
