1364be34fca0fadf8dad7a41fb105c50ceb7af3e
Ace7260 <irano7260@gmail.com>
2023-05-27 10:42:37 +0200

    adding category object

--- app/views/sport.py
+++ app/views/sport.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from django.http import request
-from app.models import Ticket
+from app.models import Ticket,Category
 
 def index(request):
     tickets=Ticket.objects.all()
+    categories=Category.objects.all()
+    
     return render(
         request,
         'app/sports/index.html',
         {
-            'tickets':tickets
+            'tickets':tickets,
+            'categories':categories
         }
     )
     
