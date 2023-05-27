cb9dbc049a932840fbc90e45690360bf63c3f3b3
DrL <dushimerosine1@gmail.com>
2023-05-27 09:46:12 +0200

    define path for ticket_index

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from app.views import home,sport
+from app.views import home,sport,ticket
 urlpatterns = [
     path('',home.index,name='home_index' ),
     # urls for sport
     path('sports',sport.index,name='sport_index'),
+    # urls for ticket
+    path('tickets',ticket.index,name='ticket_index'),
 ]
