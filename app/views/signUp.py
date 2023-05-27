07a881073a7072fc434f6fab5e0157b7d54f08c3
Negalbert <sammynegalbert2@gmail.com>
2023-05-27 11:33:33 +0200

    fonction pour enregistrer un utilisateur

--- app/views/signUp.py
+++ app/views/signUp.py
@@ -9,3 +9,20 @@ def index(request):
     'app/Auth/signUp.html'
   )
 
+def save(request):
+    if request.method == 'POST':
+        pwd = request.POST.get('password')
+        pwd2 = request.POST.get('password2')
+        last_name = request.POST.get('last_name')
+        first_name = request.POST.get('first_name')
+        email = request.POST.get('email')
+        username = request.POST.get('username')
+        if(pwd != pwd2):
+            messages.error(request,'Password incorrect')
+            return redirect('/signUp')
+        user = User.objects.create(last_name=last_name, first_name=first_name, username=username, email=email, password=pwd)
+        if user :
+            return redirect('/login')
+        else: 
+            messages.error(request,"erreur d'enregistrement")
+            return redirect('/signUp')
