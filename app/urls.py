39ab3e62b5d322b99144086a5ea43baefd433395
Negalbert <sammynegalbert2@gmail.com>
2023-05-27 11:34:51 +0200

    ajout du token

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import home
+from .views import home,logout,login, signUp
 urlpatterns = [
     path('',home.index,name='home_index' ),
     # ============== login page =============
