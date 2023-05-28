fb9146e9afb9cc0ec50138860304a1e54c2b470e
deldodai <dodaidzayn@gmail.com>
2023-05-28 13:25:18 +0200

    add path of category_index & add_category

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from .views import home,category
 urlpatterns = [
     path('',home.index,name='home_index' ),
-    path('category/',category.index,name='category_index'),
-    path('category/add/',category.add_index,name='add_category'),
+    path('category/',category.index,name='category_index' ),
 ]
