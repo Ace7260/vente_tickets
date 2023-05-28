d521e9c76d645c08071809966b2deca58b626df5
DrL <dushimerosine1@gmail.com>
2023-05-28 12:32:13 +0200

    adding setting

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from app.views import home,sport,ticket
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('',home.index,name='home_index' ),
     # urls for sport
@@ -8,4 +10,4 @@ urlpatterns = [
     path('tickets',ticket.index,name='ticket_index'),
     # urls for add ticket
     path('tickets/add', ticket.add, name="ticket_add"),
-]
+] + static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
