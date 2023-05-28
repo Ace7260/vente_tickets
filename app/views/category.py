d3ae221bfcf74e1af911b762c5721e5a74708a21
deldodai <dodaidzayn@gmail.com>
2023-05-28 13:30:54 +0200

    add the view method for adding category

--- app/views/category.py
+++ app/views/category.py
@@ -1,11 +1,8 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render
 from django.http import request
-from app.models import *
 
 def index(request):
-  categories= Category.objects.all()
-  return render(request,'app/category/index.html',{'categories':categories})
+  return render(request,'app/category/index.html')
 
 def add_index(request):
-  
   return render(request,'app/category/add_category.html')
