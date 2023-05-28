feae2bd37bc9449a2c814f60658977efbb5d1789
deldodai <dodaidzayn@gmail.com>
2023-05-28 09:46:05 +0200

    Add path to history

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from app.views import home,sport,ticket
+from app.views import home,sport,ticket,orders
 urlpatterns = [
     path('',home.index,name='home_index' ),
     # urls for sport
     path('sports',sport.index,name='sport_index'),
     # urls for ticket
     path('tickets',ticket.index,name='ticket_index'),
+  
+    path('orders',orders.index,name='orders_index'),
+    
+    
 ]
