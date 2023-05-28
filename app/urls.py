d4fe2927200f8d7304ade9ace7a74b514b24962e
deldodai <dodaidzayn@gmail.com>
2023-05-28 13:29:10 +0200

    add path for adding category

--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,5 @@ from .views import home,category
 urlpatterns = [
     path('',home.index,name='home_index' ),
     path('category/',category.index,name='category_index' ),
+    path('category/add/',category.add_index,name='add_category' ),
 ]
