7c60f7c9a9430da8f1c050db3c73083ac464c2a3
EddyNobel <enirakoze@asystresources.com>
2023-05-30 18:52:39 +0200

    defining view for searching by category

--- app/views/sport.py
+++ app/views/sport.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import request
+from django.http import request,HttpResponse
 from app.models import Ticket,Category
 
 def index(request):
@@ -14,4 +14,16 @@ def index(request):
             'categories':categories
         }
     )
-    
+def search(request):
+    search_query = request.GET.get('q')
+    if search_query:
+        # category = search_query
+        # categories = Category.objects.filter(description=category)
+        # print(categories)
+        tickets = Ticket.objects.filter(categorie__description=search_query)
+        print(tickets,'======')
+        return render(request, 'app/sports/index.html', { 'tickets': tickets})
+    else:
+        categories = Category.objects.all()
+        tickets = Ticket.objects.all()
+        return render(request, 'app/sports/index.html', {'categories': categories, 'tickets': tickets})
