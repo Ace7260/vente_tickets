07a5f264e0f33080227d035b00886b9997eae773
EddyNobel <enirakoze@asystresources.com>
2023-05-31 08:17:31 +0200

    adding login_required decorator

--- app/views/sport.py
+++ app/views/sport.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import request
 from app.models import Ticket,Category
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='login_index')
 def index(request):
     tickets=Ticket.objects.all()
     categories=Category.objects.all()
