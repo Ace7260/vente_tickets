4a4432d4479f550eb578b120c56ae504142b9f15
DrL <dushimerosine1@gmail.com>
2023-06-08 20:30:15 +0200

    code for crud operation for ticket

--- app/views/ticket.py
+++ app/views/ticket.py
@@ -1,60 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import request,HttpRequest
 from app.models import Ticket,Category
+from app.forms import TicketForm
 
 def index(request):
-    categories=Category.objects.all()
-    context={
-        'categories':categories
-    }
-    print(context)
-    return render(
-        request,
-        'app/tickets/add.html',
-        context
-    )
-# Save a new category
-
-from django.http import HttpRequest
-from django.shortcuts import redirect, render
-from django.contrib import messages
-
-from app.models import Category
-
-# Categories list
-def index(request):
-    assert isinstance(request, HttpRequest)
-    categories = Category.objects.all()
-    return render(
-        request,
-        'app/tickets/add.html',
-        {
-            'categories': categories
-        }
-    )
-
-# Add a new category
-def add(request):
-    assert isinstance(request, HttpRequest)
+    tickets = Ticket.objects.all()
+  
+    context = {'tickets': tickets}
+    
+    return render(request, 'app/tickets/index.html', context)
+  
+  
+def detail(request, pk):
+    eachticket = Ticket.objects.get(id=pk)
+  
+    context = {'eachticket': eachticket}
+    
+    return render(request, 'app/tickets/detail.html', context)
+  
+  
+def addTicket(request):
+    form = TicketForm()
+    
     if request.method == 'POST':
-        description=request.POST.get('description')
-        place=request.POST.get('place')
-        price=request.POST.get('price')
-        image=request.POST.get('image')
-        exp=request.POST.get('exp')
-        category=request.POST.get('category')
-        tick=Ticket(
-            description=description,
-            place=place,
-            price=price,
-            image=image,
-            status=True,
-            expiration=exp,
-            categorie_id=category
-        )
-        tick.save()
-    return render(
-        request,
-        'app/tickets/add.html'
-    )
-
+        form = TicketForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('ticket_index')
+    else:
+        form = TicketForm()
+        
+    context = {'form': form}
+    
+    return render(request, 'app/tickets/add.html', context)
+  
+def editTicket(request, pk):
+    ticket = Ticket.objects.get(id=pk)
+    
+    form = TicketForm(instance=ticket)
+    
+    if request.method == 'POST':
+        form = TicketForm(request.POST, request.FILES, instance=ticket)
+        if form.is_valid():
+            form.save()
+            return redirect('ticket_index')
+        
+    context = {'form': form}
+    
+    return render(request, 'app/tickets/edit.html', context)
+  
+def deleteTicket(request, pk):
+    ticket = Ticket.objects.get(id=pk)
+    ticket.delete()
+    return render(request, 'ticket_index')
