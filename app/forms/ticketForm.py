f5e85ba944d9b1c882c25095be7c1ea3adb40800
DrL <dushimerosine1@gmail.com>
2023-06-14 18:11:52 +0200

    remove unnecessary code

--- app/forms/ticketForm.py
+++ app/forms/ticketForm.py
@@ -11,11 +11,6 @@ class TicketForm(forms.ModelForm):
           'place': forms.TextInput(attrs={'class': 'form-control'}),
           'price': forms.TextInput(attrs={'class': 'form-control'}),
           'image': forms.FileInput(attrs={'class': 'form-control'}),
-<<<<<<< HEAD
           'status': forms.CheckboxInput(attrs={'class': 'checkbox'}),
           'expiration': forms.DateTimeInput(attrs={'class': 'form-control'}),
-=======
-          # 'status': forms.CheckboxInput(attrs={'class': 'form-control'}),
-          # 'expiration': forms.DateInput(attrs={'class': 'form-control'}),
->>>>>>> 7a3eefcbe9db61f210814812c518be2f7fec8c39
-        }
+          }
