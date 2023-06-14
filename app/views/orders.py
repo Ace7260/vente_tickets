223f4676e477e3a5bfc66f0cde2f3d48a595646b
Ace7260 <irano7260@gmail.com>
2023-06-14 22:27:45 +0200

    update db

--- app/views/orders.py
+++ app/views/orders.py
@@ -3,7 +3,7 @@ from django.http import HttpRequest
 from app.models import *
 from django.contrib.auth.decorators import login_required
 
-@login_required(login_url='login_index')
+# @login_required(login_url='login_index')
 def index(request):
     tickets=Ticket.objects.all()
     return render(request,'app/orders/index.html',{'tickets':tickets})
