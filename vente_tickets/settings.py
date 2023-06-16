29b96784e9cd7102c091b0ea7d12818610a8efb9
Ace7260 <irano7260@gmail.com>
2023-06-17 00:55:17 +0200

    setting mysql db

--- vente_tickets/settings.py
+++ vente_tickets/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'vente_tickets.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'vente_tickets_db',
+        'USER': 'ace1',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
