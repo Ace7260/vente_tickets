3da794b19286fa1361d364d1f54a5b5b86c0653f
EddyNobel <enirakoze@asystresources.com>
2023-06-16 19:35:42 +0200

    defining fields for ticket

--- app/forms/ticketForm.py
+++ app/forms/ticketForm.py
@@ -4,13 +4,13 @@ from app.models import Ticket, Category
 class TicketForm(forms.ModelForm):
     class Meta:
         model = Ticket
-        fields = "__all__"
+        fields = ['description','place','price','date_ajout','expiration','categorie','image','status']
         widgets = {
           'categorie': forms.Select(attrs={'class': 'form-control'}),
           'description': forms.TextInput(attrs={'class': 'form-control'}),
           'place': forms.TextInput(attrs={'class': 'form-control'}),
           'price': forms.TextInput(attrs={'class': 'form-control'}),
           'image': forms.FileInput(attrs={'class': 'form-control'}),
-          'status': forms.CheckboxInput(attrs={'class': 'checkbox'}),
-          'expiration': forms.DateTimeInput(attrs={'class': 'form-control'}),
-          }
+           'expiration': forms.DateInput(attrs={'class': 'form-control','min': '2023-01-01','type':'date'}),
+           'date_ajout': forms.DateInput(attrs={'class': 'form-control','min': '2023-01-01','type':'date'}),
+        }
