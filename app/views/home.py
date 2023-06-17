348343d80e3e6f3047d09e46052a8d137af29e1f
Ace7260 <irano7260@gmail.com>
2023-06-17 07:29:38 +0200

    define view for counting user

--- app/views/home.py
+++ app/views/home.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import request
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from app.models import User
 
 def index(request):
   return render(
     request,
     'app/home/index.html'
   )
+
+def count_users(request):
+    total_users = User.objects.count()
+    return render(request, "app/home/index.html", {"total_users": total_users})
