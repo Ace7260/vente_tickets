d5b7faedcd7b313b061086a9eead32ff8b0d3eb7
Ace7260 <irano7260@gmail.com>
2023-06-20 23:39:48 +0200

    adding url for authentication

--- vente_tickets/urls.py
+++ vente_tickets/urls.py
@@ -19,4 +19,5 @@ from django.urls import include,path
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path('',include('app.urls')),
+    path ('', include('django.contrib.auth.urls')),
 ]
