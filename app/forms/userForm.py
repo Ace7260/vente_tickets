c3c6cced95e485b872a4891e793d21828ce3aa4e
Ace7260 <irano7260@gmail.com>
2023-06-20 23:22:34 +0200

    defining userForm

---
+++ app/forms/userForm.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class UserForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = [
+            'first_name',
+            'last_name',
+            'email',
+            'username',
+            'password1',
+            'password2'
+        ]
