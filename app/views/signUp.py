97fc9032950e0a0abc290959ce12ba474c39413a
Ace7260 <irano7260@gmail.com>
2023-06-20 23:38:27 +0200

    new form for registration

--- app/views/signUp.py
+++ app/views/signUp.py
@@ -2,30 +2,27 @@ from django.shortcuts import render,  redirect
 from django.http import request
 from django.contrib.auth.models import User
 from django.contrib import messages
+from app.forms import UserForm
 
 def index(request):
+  form = UserForm()
+  context={
+      'form':form
+  }
   return render(
     request,
-    'app/Auth/signUp.html'
+    'app/Auth/signUp.html',context
   )
 
 def save(request):
-    if request.method == 'POST':
-        pwd = request.POST.get('password')
-        pwd2 = request.POST.get('password2')
-        last_name = request.POST.get('last_name')
-        first_name = request.POST.get('first_name')
-        email = request.POST.get('email')
-        username = request.POST.get('username')
-        if(first_name == "" and last_name == "" and email == "" and username == ""):
-            messages.error(request, "Please fill in all fields !")
-            return redirect('/signUp')
-        if(pwd != pwd2):
-            messages.error(request,'Passwords incorrect')
-            return redirect('/signUp')
-        user = User.objects.create(last_name=last_name, first_name=first_name, username=username, email=email, password=pwd)
-        if user :
-            return redirect('/login')
-        else: 
-            messages.error(request,"Failed to save")
-            return redirect('/signUp')
+      if request.method == 'POST':
+        form = UserForm(request.POST)
+        print(form)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            # group = Group.objects.get(name = 'employees')
+            # user.groups.add(group)    
+            messages.success(request, 'Le compte de '+username+' a été ajouté')
+            return redirect('')
+        return redirect('/signUp')
