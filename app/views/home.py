be5ad7a6a2c1a86c63819085faf45d12daa89480
Ace7260 <irano7260@gmail.com>
2023-06-23 22:45:41 +0200

    displaying number of users

--- app/views/home.py
+++ app/views/home.py
@@ -5,7 +5,8 @@ from dotenv import load_dotenv
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.views.generic import TemplateView
-from app.models import User
+from app.models import Ticket
+from django.contrib.auth.models import User 
 
 load_dotenv()
 
@@ -14,8 +15,13 @@ token_key = os.getenv("TOKEN_KEY")
 
 @login_required(login_url='login_index')
 def index(request):
-  
+     total_user=User.objects.all().count()
+     total_ticket=Ticket.objects.all().count()
+     context={
+       'total_user':total_user,
+       'total_ticket':total_ticket
+     }
      return render(
       request,
-      'app/home/index.html'
+      'app/home/index.html',context
     )
